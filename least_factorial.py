f8d6e0ff6089aedc2057aee1bf2f339ae772fc89
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2016-10-20 16:30:48 +0300

    Add solution for least_factorials

---
+++ least_factorial.py
@@ -0,0 +1,8 @@
+def least_factorial(n):
+    k = 1
+    m = 1
+    while True:
+        # The invariant here is that k == (m-1)!
+        if k >= n: return k
+        m += 1
+        k *= m
---
+++ test_least_factorial.py
@@ -0,0 +1,19 @@
+import unittest
+import least_factorial as T
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(T.least_factorial(1), 1)
+        self.assertEqual(T.least_factorial(2), 2)
+        self.assertEqual(T.least_factorial(3), 6)
+        self.assertEqual(T.least_factorial(5), 6)
+        self.assertEqual(T.least_factorial(17), 24)
+        self.assertEqual(T.least_factorial(42), 120)
+        self.assertEqual(T.least_factorial(120), 120)
+        self.assertEqual(T.least_factorial(121), 720)
+
+        self.assertEqual(T.least_factorial(0), 1)
+
+if __name__ == '__main__':
+    unittest.main()
