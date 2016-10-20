477160a2a3be43ba09856e06e0121a8215e52486
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2016-10-20 17:19:28 +0300

    Add new test for least_factorial
    On an sunny afternoon of October
    I had a bright idea.
    Here it is.

--- test_least_factorial.py
+++ test_least_factorial.py
@@ -13,6 +13,8 @@ class Test(unittest.TestCase):
         self.assertEqual(T.least_factorial(120), 120)
         self.assertEqual(T.least_factorial(121), 720)
 
+        self.assertEqual(T.least_factorial(1000), 5040)
+
         self.assertEqual(T.least_factorial(0), 1)
 
 if __name__ == '__main__':
